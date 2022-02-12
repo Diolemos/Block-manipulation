896b59988307d2f1c42f34bd924b956d2b68e863
Dio Lemos <pedrodiogenes@outlook.com>
2022-02-12 17:27:38 +0000

    Position x
    Created the variable and event listiner to control de blocks position X ( style left property).
    
    in HTML, changed the input pos-x default value to 0

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ let posY = document.getElementById("pos-y");
 posY.addEventListener('input', function () {
     block.style.top = posY.value + "px"
 })
+
+// Position X manipulation
+
+let posX = document.getElementById('pos-x');
+
+posX.addEventListener('input', function () {
+    block.style.left = posX.value + "px";
+})
